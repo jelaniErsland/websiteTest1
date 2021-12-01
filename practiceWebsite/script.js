a2006e0750bb4dbddef8fb1cd992baf9a056062d
jelaniErsland <95370802+jelaniErsland@users.noreply.github.com>
2021-12-01 09:40:01 -0700

    Add files via upload

---
+++ practiceWebsite/script.js
@@ -0,0 +1,55 @@
+const soda = prompt(`What is your favorite soda to drink?`);
+
+if (soda.toLowerCase() === "pepsi") {
+  document.querySelector(".moving-text").textContent =
+    "Pepsi makes me puke you fag. 🤮";
+} else if (soda.toLowerCase() === "coke") {
+  document.querySelector(".moving-text").textContent =
+    "Coke is so good, it makes me shiver! 🥳";
+} else if (soda.toLowerCase() === "dr pepper") {
+  document.querySelector(".moving-text").textContent =
+    "Dr Pepper fizzles in so many good ways yeah! 🍾";
+} else if (soda.toLowerCase() === "coca-cola") {
+  document.querySelector(".moving-text").textContent =
+    "Ooh you're fancy, saying Coca-Cola instead of a normal American! 😏";
+} else if (soda.toLowerCase() === "fanta") {
+  document.querySelector(
+    ".moving-text"
+  ).textContent = `In my land, we call that "orange soda." Don't forget to call about our dinner date! 💃🏻`;
+} else if (soda.toLowerCase() === "sprite") {
+  document.querySelector(".moving-text").textContent =
+    "Have you ever had McDonald's Sprite? It'll make your brain orgasm twice... 🥵";
+} else if (soda.toLowerCase() === "7up") {
+  document.querySelector(".moving-text").textContent =
+    "Heads up 7 up! What the heck does that mean? 👍🏻";
+} else if (soda.toLowerCase() === "7 up") {
+  document.querySelector(".moving-text").textContent =
+    "Heads up 7 up! What the heck does that mean? 👍🏻";
+} else if (soda.toLowerCase() === "7-up") {
+  document.querySelector(".moving-text").textContent =
+    "Heads up 7 up! What the heck does that mean? 👍🏻";
+} else if (soda.toLowerCase() === "coca cola") {
+  document.querySelector(".moving-text").textContent =
+    "Ooh you're fancy, saying Coca-Cola instead of a normal American! 😏";
+} else if (soda.toLowerCase() === "bacon") {
+  document.querySelector(".moving-text").textContent =
+    "That's cheating. You just read what you saw on that picture! Naughty boi. 🤚🏻";
+} else if (soda.toLowerCase() === "rootbeer") {
+  document.querySelector(".moving-text").textContent =
+    "Makes me think of all the dumb kids who pretend it's real beer. You're an idiot. 😡";
+} else if (soda.toLowerCase() === "ginger ale") {
+  document.querySelector(".moving-text").textContent =
+    "The fancy man's Sprite. I'm going to take your head and make it into a shelf. ⚔️";
+} else if (soda.toLowerCase() === "gingerale") {
+  document.querySelector(".moving-text").textContent =
+    "The fancy man's Sprite. I'm going to take your head and make it into a shelf. ⚔️";
+} else if (soda.toLowerCase() === "lemonade") {
+  document.querySelector(".moving-text").textContent =
+    "That is not a soda. Say hello to hell for me. I'll see ya down there. 🔥";
+} else if (soda.toLowerCase() === "mountain dew") {
+  document.querySelector(".moving-text").textContent =
+    "Why do the people who like Mountain Dew talk about Mountain Dew so freaking much? Next. 🙄";
+} else {
+  document.querySelector(".moving-text").textContent =
+    "I don't think that's a soda. Reload the page and either check your spelling or say something better.";
+}
